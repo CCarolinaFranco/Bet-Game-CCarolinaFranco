d80a4b7c95a4af20ffd49b26a345a42a0a7e198d
CCarolinaFranco <cirilina@hotmail.com>
2023-05-31 22:36:33 +0100

    random num and select

--- JS/main.js
+++ JS/main.js
@@ -4,40 +4,42 @@
 const selectNum = document.querySelector('.js_select_num');
 const inputApuestas = document.querySelector('.js_inp_apuestas');
 const btnJugar = document.querySelector('.js_btn-jugar');
-
+const ganadoP = document.querySelector('.js_Pganado')
 
 
 const selectNumValue = selectNum.value;
 const inputApuestasValue = inputApuestas.value;
-function selectYapuesta(){
-      
-
-    if(selectNumValue = inputApuestasValue);
-     { 
-
-     }
-     
-
-}
-
-
-/*function inputA(){
-    
-   
-    if(inputApuestasValue =  < 50) {
-      
-
-   }
-
-
-}*/
+const numSelecV =  parseInt(selectNum.value);
+console.log(numSelecV)
 
 
 
 function clickBtnJugar(){
-    selectYapuesta()
-
-
+    
+    getRandomNumber()
+    
 }
 
 btnJugar.addEventListener("click",clickBtnJugar);
+
+const numRand = Math.ceil(Math.random() *6);
+//console.log(numRand);
+
+function getRandomNumber(max){ 
+    //event.preventDefault();
+    return Math.ceil(Math.random() * max);
+    
+   
+   
+   
+     
+   ganadoP.innerHTML = `<p>Has ganado el doble de lo apostado</p> `
+
+
+   }
+
+
+
+
+  
+
