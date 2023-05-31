f80078ccfbf0f9e140d24721a0ccf93f28ad6055
CCarolinaFranco <cirilina@hotmail.com>
2023-05-31 10:27:02 +0100

    evaluacion unico push

---
+++ JS/main.js
@@ -0,0 +1,43 @@
+"use strict";
+
+
+const selectNum = document.querySelector('.js_select_num');
+const inputApuestas = document.querySelector('.js_inp_apuestas');
+const btnJugar = document.querySelector('.js_btn-jugar');
+
+
+
+const selectNumValue = selectNum.value;
+const inputApuestasValue = inputApuestas.value;
+function selectYapuesta(){
+      
+
+    if(selectNumValue = inputApuestasValue);
+     { 
+
+     }
+     
+
+}
+
+
+/*function inputA(){
+    
+   
+    if(inputApuestasValue =  < 50) {
+      
+
+   }
+
+
+}*/
+
+
+
+function clickBtnJugar(){
+    selectYapuesta()
+
+
+}
+
+btnJugar.addEventListener("click",clickBtnJugar);
